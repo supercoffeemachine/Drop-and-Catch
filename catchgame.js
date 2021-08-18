6dff44bc618425152f4272ee05885c187def89df
supercoffeemachine <88757809+supercoffeemachine@users.noreply.github.com>
2021-08-18 22:44:45 +0800

    Add files via upload

---
+++ catchgame.js
@@ -0,0 +1,37 @@
+let basket = document.querySelector('.basket');        //movement of basket in progres
+let move = 10;
+const left_arrow = 20;
+const right_arrow = 20;
+var score = 0;
+const life = 3;
+
+function moveBasket(event){
+ car
+
+}
+
+
+
+
+function start(){      //starting the game 
+
+
+}
+
+function endgame(){   //game end screen
+
+
+
+
+}
+
+
+function itemfall(){  //item falling 
+  
+}
+
+function checkitem(){ //collision 
+
+
+
+}
